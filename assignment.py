15961f050b08ee7431207fe462ea3cde6c0464d5
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2023-10-11 21:02:30 +0100

    Update to assignment 1 code
    A string is an array of characters.

--- assignment.py
+++ assignment.py
@@ -1,19 +1,24 @@
 myStr = "string value"
-strList = list(myStr)
-x = 0
-y = 0
-count = 0
+# strList = list(myStr)
 
 # Question 1
+x = 0
+
 while x < len(myStr):
-  print(strList[x])
+  # print(strList[x])
+  print(myStr[x])
   x += 1
   
 # Question 2
+y = 0
+count = 0
+
 while y < len(myStr):
-  if strList[y] == "a" or strList[y] == "e" or strList[y] == "i" or strList[y] == "o" or strList[y] == "u":
+  # if strList[y] == "a" or strList[y] == "e" or strList[y] == "i" or strList[y] == "o" or strList[y] == "u":
+  if myStr[y].lower() in ["a", "e", "i", "o", "u"]:
     count = count + 1
   y += 1
+
 print(count)
 
 # Question3
